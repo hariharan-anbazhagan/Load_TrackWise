d9dcd863b5a25e96af989ddd5d6257031b826fbf
hariharan-anbu <anbuhari200@gmail.com>
2025-06-03 22:50:38 +0530

    new service has been updated

---
+++ Service/Load-service/src/main/java/com/hariSolutions/Load_service/LoadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hariSolutions.Load_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadServiceApplication.class, args);
+	}
+
+}
---
+++ Service/Load-service/src/test/java/com/hariSolutions/Load_service/LoadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hariSolutions.Load_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
